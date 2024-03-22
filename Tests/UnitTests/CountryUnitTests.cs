e6635981a796a852c60c38849d0d87c6ce5c4028
Tommy Blaires <tjbpr24@gmail.com>
2024-03-21 23:05:30 -0300

    start unit test

--- Tests/UnitTests/CountryUnitTests.cs
+++ Tests/UnitTests/CountryUnitTests.cs
@@ -1,7 +1,12 @@
+using Application.Interfaces;
+using Moq;
+
 namespace Tests.UnitTests
 {
     public class CountryUnitTests
     {
+        Mock<ICountryService> mock = new Mock<ICountryService>();
+
         [Fact]
         public void Test1()
         {
